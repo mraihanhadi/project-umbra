b6d0695d9009fe3c3b7498918162a62ef51ac4e8
Raihan Hadi <114351863+mraihanhadi@users.noreply.github.com>
2025-07-21 13:44:20 +0700

    Menambahkan Menu dan sprite menu
    Menambahkan sprite untuk menu seperti tombol menu, resume, save, load, dan settings. Dan juga menambahkan fitur untuk menutup dan juga membuka menu menggunakan tombol escape. Untuk menutup save menu, load menu, dan settings menu bisa menggunakan escape

--- project-umbra/Assets/Script/Menu.cs
+++ project-umbra/Assets/Script/Menu.cs
@@ -1,25 +1,90 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MenuOpener : MonoBehaviour
 {
     public GameObject menu;
     public GameObject mainView;
-
+    GameObject menuBtn;
+    GameObject saveMenu;
+    GameObject loadMenu;
+    GameObject settingsMenu;
     void Start()
     {
+        menuBtn = menu.transform.Find("MenuBtn").gameObject;
+        saveMenu = menu.transform.Find("SaveMenu").gameObject;
+        loadMenu = menu.transform.Find("LoadMenu").gameObject;
+        settingsMenu = menu.transform.Find("SettingsMenu").gameObject;
+        menuBtn.SetActive(true);
+        saveMenu.SetActive(false);
+        loadMenu.SetActive(false);
+        settingsMenu.SetActive(false);
         mainView.SetActive(true);
         menu.SetActive(false);
     }
 
-    public void openMenu()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!menu.activeInHierarchy) //Jika menu tidak aktif
+            {
+                OpenMenu();
+            }
+            else
+            {
+                if (!menuBtn.activeInHierarchy)
+                {
+                    Debug.Log(55);
+                    BackToMenu();
+                }
+                else
+                {
+                    CloseMenu();
+                }
+            }
+        }
+    }
+
+    public void OpenMenu()
     {
         mainView.SetActive(false);
         menu.SetActive(true);
     }
 
-    public void closeMenu()
+    public void CloseMenu()
     {
         mainView.SetActive(true);
         menu.SetActive(false);
+    }
+
+    void BackToMenu()
+    {
+        saveMenu.SetActive(false);
+        loadMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        mainView.SetActive(false);
+        menuBtn.SetActive(true);
+    }
+
+    public void OpenSaveMenu()
+    {
+        menuBtn.SetActive(false);
+        saveMenu.SetActive(true);
+        mainView.SetActive(true);
+    }
+
+    public void OpenLoadMenu()
+    {
+        menuBtn.SetActive(false);
+        loadMenu.SetActive(true);
+        mainView.SetActive(true);
+    }
+
+    public void OpenSettingsMenu()
+    {
+        menuBtn.SetActive(false);
+        settingsMenu.SetActive(true);
+        mainView.SetActive(true);
     }
 }
