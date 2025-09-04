ca46d66919d45e071ea3287752371a10b924a69f
Raihan Hadi <114351863+mraihanhadi@users.noreply.github.com>
2025-09-04 20:32:28 +0700

    Fixed loading screen error

--- project-umbra/Assets/Script/loadingScreen.cs
+++ project-umbra/Assets/Script/loadingScreen.cs
@@ -40,7 +40,10 @@ public class loadingScreen : MonoBehaviour
         // When done, load next scene
         if (elapsedTime >= scrollDuration)
         {
-            GameManager.Instance.timeManager.ResumeTime();
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.timeManager.ResumeTime();
+            }
             SceneManager.LoadScene(nextSceneName);
         }
     }
